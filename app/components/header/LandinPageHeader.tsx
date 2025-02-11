ecaa22ed2f46194db93fe1969b3e257def2b9c2f
marcelo--rodrigues2@hotmail.com <marcelo--rodrigues2@hotmail.com>
2025-02-11 15:38:23 -0300

    chore: created landing page header

---
+++ app/components/header/LandinPageHeader.tsx
@@ -0,0 +1,9 @@
+export default function LandingPageHeader() {
+  return (
+    <div className="border border-red-700">
+      <div>Titulo</div>
+      <div>Foto</div>
+      <div>Texto</div>
+    </div>
+  );
+}
