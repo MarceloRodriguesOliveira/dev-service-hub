1a0767b8f31d0838a3f7bccabcfd4a1d4568a818
marcelo--rodrigues2@hotmail.com <marcelo--rodrigues2@hotmail.com>
2025-02-12 21:34:31 -0300

    style: updated main components on home

--- app/components/header/LandingPageHeader.tsx
+++ app/components/header/LandingPageHeader.tsx
@@ -5,8 +5,8 @@ import LottieConstructionAnimation from "../lottieanimations/lottieConstructionA
 export default function LandingPageHeader() {
   return (
     <>
-      <div className="order-first  flex flex-col justify-center h-full  ">
-        <h1 className="sm:flex-col md:flex text-5xl lg:text-6xl  font-grotesk font-bold h-full md:w-fit leading-none md:leading-tight">
+      <div className="order-first  flex flex-col justify-center h-full ">
+        <h1 className="sm:flex-col md:flex text-5xl lg:text-6xl  font-grotesk font-bold h-full md:w-fit leading-none md:leading-tight lg:mb-9 ">
           Navigating the
           <br className="hidden lg:inline-block" /> Digital Landscape
           <br className="hidden lg:inline-block" /> for success
@@ -18,8 +18,8 @@ export default function LandingPageHeader() {
             className="w-full h-64 scale-150"
           />
         </div>
-        <div className="order-last flex flex-col justify-between gap-5 lg:gap-9  h-full  lg:pt-9 ">
-          <p className="text-xl md:leading-7 text-black ">
+        <div className="order-last flex flex-col justify-between h-full gap-5 md:gap-0  ">
+          <p className="text-xl md:leading-7 text-black   md:mb-9 ">
             Our digital marketing agency helps businesses
             <br className="hidden lg:inline-block" /> grow and succeed online
             through a range of
@@ -27,14 +27,14 @@ export default function LandingPageHeader() {
             PPC, social media marketing,
             <br className="hidden lg:inline-block" /> and content creation.
           </p>
-          <div className="">
+          <div>
             <button className=" bg-slate-900 text-xl md:leading-7 text-white font-grotesk px-9 py-5 rounded-[14px] w-full md:w-[264px] ">
               Saiba mais
             </button>
           </div>
         </div>
       </div>
-      <div className=" hidden lg:flex justify-center items-center h-full order-2 ">
+      <div className=" hidden lg:flex justify-center items-center h-full order-2   ">
         <LottieConstructionAnimation />
       </div>
     </>
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
 import Image from "next/image";
 import Navbar from "./components/navbar/navbar";
 import LandingPageHeader from "./components/header/LandingPageHeader";
+import Label from "./components/label/Label";
+import ServiceSection from "./components/servicesection/ServiceSection";
 
 export default function Home() {
   return (
     <div className="min-h-screen px-4 md:px-16 xl:px-24">
       <Navbar />
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <section className="w-full  grid grid-cols-1 lg:grid-cols-2 justify-between items-stretch md:text-left my-8 md:my-16 ">
+        <section className="w-full  grid grid-cols-1 lg:grid-cols-2 justify-between items-stretch md:text-left my-8 md:my-16  ">
           <LandingPageHeader />
+        </section>
+        <section className="w-full">
+          <ServiceSection />
         </section>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
