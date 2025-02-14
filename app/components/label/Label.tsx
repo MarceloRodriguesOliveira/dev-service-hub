61c1b9ab85a048f2fb561a6b5ab2b2dd4d090b2b
marcelo--rodrigues2@hotmail.com <marcelo--rodrigues2@hotmail.com>
2025-02-14 15:21:40 -0300

    style: removed useless containers

--- app/components/label/Label.tsx
+++ app/components/label/Label.tsx
@@ -1,9 +1,9 @@
 export default function Label({ text }: { text: string }) {
   return (
-    <div className="">
+    <>
       <h2 className="font-grotesk text-[40px] px-2 text-black font-medium w-fit bg-limegreen rounded-lg ">
         {text}
       </h2>
-    </div>
+    </>
   );
 }
--- app/components/label/LabelText.tsx
+++ app/components/label/LabelText.tsx
@@ -4,10 +4,10 @@ type textProp = {
 
 export default function LabelText({ children }: textProp) {
   return (
-    <div className="border-blue-300">
+    <>
       <p className="text-lg front-grotesk text-center md:text-left">
         {children}
       </p>
-    </div>
+    </>
   );
 }
