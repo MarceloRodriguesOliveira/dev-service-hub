e1811a362f017b97c3d660c008b6d2f992889187
marcelo--rodrigues2@hotmail.com <marcelo--rodrigues2@hotmail.com>
2025-02-17 21:09:12 -0300

    style: cards finished

--- app/components/card/card.tsx
+++ app/components/card/card.tsx
@@ -1,6 +1,11 @@
 import LearnMore from "./learn";
 import MiniLabel from "./minilabel";
 import Magnifier from "@/public/tokyo-magnifier-web-search-with-elements 2.svg";
+import BrowserWindow from "@/public/tokyo-selecting-a-value-in-the-browser-window 1.svg";
+import EmoticonWindow from "@/public/tokyo-browser-window-with-emoticon-likes-and-stars-around 2.svg";
+import MessageToAnother from "@/public/tokyo-sending-messages-from-one-place-to-another 1.svg";
+import InformationWindows from "@/public/tokyo-many-browser-windows-with-different-information 1.svg";
+import GraphIllustatrion from "@/public/tokyo-volumetric-analytics-of-different-types-in-web-browsers 2.svg";
 
 export default function Card({
   upperText,
@@ -11,15 +16,32 @@ export default function Card({
   bottomText: string;
   index: number;
 }) {
+  const iconTagHandler = (index: number) => {
+    switch (index) {
+      case 0:
+        return <Magnifier />;
+      case 1:
+        return <BrowserWindow />;
+      case 2:
+        return <EmoticonWindow />;
+      case 3:
+        return <MessageToAnother />;
+      case 4:
+        return <InformationWindows />;
+      case 5:
+        return <GraphIllustatrion />;
+    }
+  };
+
   return (
     <>
       <MiniLabel upperText={upperText} bottomText={bottomText} index={index} />
       <LearnMore index={index} />
       <div
-        className="absolute scale-75 lg:scale-100 right-0 bottom-0 top-28 lg:right-[50px] lg:top-[72px] 
+        className="absolute scale-[0.8] lg:scale-100 right-0 bottom-0 top-28 lg:right-[50px] lg:top-[72px] 
       "
       >
-        <Magnifier />
+        {iconTagHandler(index)}
       </div>
     </>
   );
