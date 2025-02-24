3a94b4241eb89227d1d7b467b66fdee38f6ec1b8
marcelo--rodrigues2@hotmail.com <marcelo--rodrigues2@hotmail.com>
2025-02-24 16:03:06 -0300

    style: changed font family

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="w-full border-greyed border-t-2 mt-20 text-center py-10 flex flex-col items-center justify-center gap-5">
       <div className="flex items-center justify-center gap-3">
         <CodeIcon />
-        <h4 className="text-2xl">DevServiceHub</h4>
+        <h4 className="text-2xl font-grotesk">DevServiceHub</h4>
       </div>
       <p className="text-sm">
         © 2025 <strong>DevServiceHub</strong>. Todos os direitos reservados.
