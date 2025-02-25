0a49069fd8ffd8eda823e27a1b6cc93d87cf6309
marcelo--rodrigues2@hotmail.com <marcelo--rodrigues2@hotmail.com>
2025-02-24 21:07:14 -0300

    chore: created register action for form handling

---
+++ app/(auth)/register/registerAction.ts
@@ -0,0 +1,51 @@
+"use server";
+
+import db from "@/lib/db";
+import { hashSync } from "bcrypt-ts";
+import { redirect } from "next/navigation";
+
+export default async function registerAction(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  _prevState: any,
+  formData: FormData
+) {
+  const entries = Array.from(formData.entries());
+  const data = Object.fromEntries(entries) as {
+    name: string;
+    email: string;
+    password: string;
+  };
+
+  console.log("=== Server Action Register User");
+  console.log(data);
+
+  if (!data.email || !data.name || data.password) {
+    return {
+      message: "Preencha todos os campos",
+      success: false,
+    };
+  }
+
+  const user = await db.user.findUnique({
+    where: {
+      email: data.email,
+    },
+  });
+
+  if (user) {
+    return {
+      message: "Este usuário já existe",
+      success: false,
+    };
+  }
+
+  await db.user.create({
+    data: {
+      email: data.email,
+      name: data.name,
+      password: hashSync(data.password),
+    },
+  });
+
+  return redirect("/");
+}
