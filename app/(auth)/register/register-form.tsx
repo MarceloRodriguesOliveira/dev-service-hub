c35d91f957af6c50ce20689ebfe6e41f9cda2b5c
marcelo--rodrigues2@hotmail.com <marcelo--rodrigues2@hotmail.com>
2025-02-26 20:46:12 -0300

    style: form main layout completed

--- app/(auth)/register/register-form.tsx
+++ app/(auth)/register/register-form.tsx
@@ -3,37 +3,63 @@
 import { useActionState } from "react";
 import registerAction from "./registerAction";
 import Form from "next/form";
-import { Label } from "@mui/icons-material";
-import { Button, Input } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 
 export default function RegisterForm() {
   const [state, formAction, isPending] = useActionState(registerAction, null);
+
   return (
     <>
       {state?.success === false && (
         <div
-          className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+          className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 mb-5 rounded relative"
           role="alert"
         >
           <strong className="font-bold">Erro!</strong>
           <span className="block">{state?.message}</span>
         </div>
       )}
-      <Form action={formAction}>
-        <div>
-          <Label>Nome</Label>
-          <Input type="text" name="name" placeholder="Fulano de Tal" />
+      <Form action={formAction} className="flex flex-col gap-4 ">
+        <div className="w-full">
+          <TextField
+            label="Nome"
+            variant="outlined"
+            className="w-full"
+            name="name"
+          ></TextField>
         </div>
         <div>
-          <Label>Email</Label>
-          <Input type="email" name="email" placeholder="eu@exemplo.com" />
+          <TextField
+            label="E-mail"
+            variant="outlined"
+            className="w-full"
+            name="email"
+            type="email"
+            slotProps={{
+              input: { autoComplete: "new-password" },
+            }}
+          >
+            email
+          </TextField>
         </div>
         <div>
-          <Label>Senha</Label>
-          <Input type="password" name="password" placeholder="********" />
+          <TextField
+            label="Senha"
+            variant="outlined"
+            className="w-full"
+            name="password"
+            type="password"
+            slotProps={{
+              input: { autoComplete: "off" },
+            }}
+          ></TextField>
         </div>
         <div>
-          <Button disabled={isPending} className="w-full mt-6" type="submit">
+          <Button
+            disabled={isPending}
+            className="w-full mt-6 bg-dark text-white text-center text-base "
+            type="submit"
+          >
             Registrar
           </Button>
         </div>
