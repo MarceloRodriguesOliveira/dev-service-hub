ef8c07b2d691cd9206b95194729f751ab99bc80f
marcelo--rodrigues2@hotmail.com <marcelo--rodrigues2@hotmail.com>
2025-02-26 20:47:12 -0300

    style: screen height properties updated

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,21 +1,27 @@
-import { Card, CardHeader, CardContent } from "@mui/material";
+import { Card, CardContent } from "@mui/material";
+import CardHeader from "@mui/material/CardHeader";
 import RegisterForm from "./register-form";
 import Link from "next/link";
 
 export default async function RegisterPage() {
   return (
-    <div className="flex flex-col justify-center items-center  border border-red-950 h-full py-40">
-      <Card className="max-w-sm w-full rounded-2xl mt-12 h-full">
-        <CardHeader>
-          <h2 className="text-xl font-bold">Cadastre-se</h2>
-        </CardHeader>
-        <CardContent>
+    <div className="flex flex-col justify-center items-center  min-h-screen px-4">
+      <Card className="max-w-sm w-full rounded-2xl mt-12 h-full ">
+        <CardHeader
+          title="Cadastre-se"
+          subheader="Faça seu cadastro gratuitamente."
+          slotProps={{
+            title: { color: "#191a23", fontSize: "18px", fontWeight: "700" },
+            subheader: { fontSize: "14px" },
+          }}
+        />
+        <CardContent className="py-0">
           <RegisterForm />
         </CardContent>
       </Card>
-      <p className="text-sm text-muted-foreground mt-3">
-        Já possui cadastro?{""}
-        <Link className="text-gray-800 hover:underline" href="/login">
+      <p className="text-sm text-muted-foreground text-white mt-3">
+        Já possui cadastro?{" "}
+        <Link className="text-gray-500 hover:underline" href="/login">
           Faça o login
         </Link>
       </p>
