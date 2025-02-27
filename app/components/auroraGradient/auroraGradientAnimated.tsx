3f784dcfb69eb22fee5ca26ac0ecc34a53e7f297
marcelo--rodrigues2@hotmail.com <marcelo--rodrigues2@hotmail.com>
2025-02-27 11:45:50 -0300

    style: finished aurora gradient animation

--- app/components/auroraGradient/auroraGradientAnimated.tsx
+++ app/components/auroraGradient/auroraGradientAnimated.tsx
@@ -1,14 +1,36 @@
-const STATIC_COLOR = "#13FFAA";
+"use client";
+const COLORS_TOP = ["#13FFAA", "#1E67C6", "#CE84CF", "#DD335C"];
+import {
+  motion,
+  useMotionValue,
+  useMotionTemplate,
+  animate,
+} from "motion/react";
+import { useEffect } from "react";
 
 export default function AuroraGradient({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const backgroundImage = `radial-gradient(125% 125% at 50% 20%, #020617 50%, ${STATIC_COLOR})`;
+  const color = useMotionValue(COLORS_TOP[0]);
+
+  useEffect(() => {
+    animate(color, COLORS_TOP, {
+      ease: "easeInOut",
+      duration: 10,
+      repeat: Infinity,
+      repeatType: "mirror",
+    });
+  }, [color]);
+
+  const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 20%, #020617 50%, ${color})`;
   return (
-    <section style={{ backgroundImage }} className="relative min-h-screen">
+    <motion.section
+      style={{ backgroundImage }}
+      className="relative min-h-screen"
+    >
       {children}
-    </section>
+    </motion.section>
   );
 }
