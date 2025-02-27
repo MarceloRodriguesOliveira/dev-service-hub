42db291bc47e5707d7ea067c5db8a33bdb3bb60d
marcelo--rodrigues2@hotmail.com <marcelo--rodrigues2@hotmail.com>
2025-02-27 13:44:21 -0300

    feat: added login action

---
+++ app/(auth)/login/loginAction.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import { signIn } from "@/auth";
+import { isRedirectError } from "next/dist/client/components/redirect-error";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default async function loginAction(_prevState: any, formData: FormData) {
+  try {
+    await signIn("credentials", {
+      email: formData.get("email") as string,
+      password: formData.get("password") as string,
+      redirect: true,
+      redirectTo: "/dashboard",
+    });
+
+    return { success: true };
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    if (isRedirectError(e)) {
+      throw e;
+    }
+    if (e.type === "CredentialsSignin") {
+      return { success: false, message: "Dados de login incorretos" };
+    }
+    console.error(e);
+    return { success: false, message: "Oops, algun erro occoreu" };
+  }
+}
