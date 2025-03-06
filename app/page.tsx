83ad3eba0f3e418a2ee667aaa4303167490cba54
marcelo--rodrigues2@hotmail.com <marcelo--rodrigues2@hotmail.com>
2025-03-06 13:55:56 -0300

    style: changed main div properties

--- app/page.tsx
+++ app/page.tsx
@@ -7,24 +7,28 @@ import Footer from "./components/footer/footer";
 
 export default function Home() {
   return (
-    <div>
-      <div className="min-h-screen px-4 md:px-16 xl:px-24">
-        <Navbar />
-        <main className="flex flex-col gap-[100px] row-start-2 items-center sm:items-start">
-          <section className="w-full grid grid-cols-1 lg:grid-cols-2 justify-between items-stretch md:text-left   ">
-            <LandingPageHeader />
-          </section>
-          <section className="w-full">
-            <ServiceSection />
-          </section>
-          <section className="w-full">
-            <WorkingProcessSection />
-          </section>
-          <section className="w-full ">
-            <CallToActionSection />
-          </section>
-        </main>
-      </div>
+    <div className="min-h-screen overflow-hidden ">
+      <Navbar />
+      <main className="flex flex-col gap-[100px] row-start-2 items-center sm:items-start ">
+        <section className="w-full grid grid-cols-1 lg:grid-cols-2 justify-between items-stretch md:text-left px-4 md:px-16 xl:px-24 mt-[100px]">
+          <LandingPageHeader />
+        </section>
+        <section
+          className="w-full scroll-mt-[100px] px-4 md:px-16 xl:px-24"
+          id="services"
+        >
+          <ServiceSection />
+        </section>
+        <section
+          className="w-full scroll-mt-[100px] px-4 md:px-16 xl:px-24"
+          id="proccess"
+        >
+          <WorkingProcessSection />
+        </section>
+        <section className="w-full scroll-mt-[100px] px-4 md:px-16 xl:px-24">
+          <CallToActionSection />
+        </section>
+      </main>
       <Footer />
     </div>
   );
