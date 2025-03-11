6cdb63f438a669d993c0381590094e5b7419ebae
marcelo--rodrigues2@hotmail.com <marcelo--rodrigues2@hotmail.com>
2025-03-11 18:22:26 -0300

    style: updated some components

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,15 @@
+import { Card, CardContent, CardHeader } from "@mui/material";
+import BuildMyProjectForm from "./project-form";
+
 export default function HireOurServices() {
   return (
-    <div className="flex justify-center items-center border border-red-600 min-h-screen bg-[url('/noise.svg')] bg-cover bg-center">
-      <h1 className="text-7xl">Faça o seu orçamento conosco!</h1>
+    <div className="flex flex-col justify-center items-center border border-red-600 min-h-screen bg-[url('/noise.svg')] bg-cover bg-center">
+      <Card className="max-w-sm w-full rounded-2xl mt-12 h-full ">
+        <CardHeader title="Discuta o seu projeto conosco" />
+        <CardContent className="py-0 px-4">
+          <BuildMyProjectForm />
+        </CardContent>
+      </Card>
     </div>
   );
 }
