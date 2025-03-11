c1148eb3a34c724b1ad99a6f5d641e6593843905
marcelo--rodrigues2@hotmail.com <marcelo--rodrigues2@hotmail.com>
2025-03-11 18:16:49 -0300

    chore: added file for common techs used on main form

---
+++ techs.ts
@@ -0,0 +1,37 @@
+export const frontEndTechnologies = [
+  {
+    id: 1,
+    tech: "React",
+  },
+  {
+    id: 2,
+    tech: "Next",
+  },
+  {
+    id: 3,
+    tech: "Vue",
+  },
+  {
+    id: 4,
+    tech: "Angular",
+  },
+];
+
+export const backEndTechnologies = [
+  {
+    id: 1,
+    tech: "Node",
+  },
+  {
+    id: 2,
+    tech: "Java Spring",
+  },
+  {
+    id: 3,
+    tech: "NestJs",
+  },
+  {
+    id: 4,
+    tech: "Laravel",
+  },
+];
