a36573cdc77e3be57980526cb652c3dafa1a5f4a
marcelo--rodrigues2@hotmail.com <marcelo--rodrigues2@hotmail.com>
2025-03-13 11:53:27 -0300

    chore: changed TextField for Select on  inputs

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,7 +5,10 @@ export default function HireOurServices() {
   return (
     <div className="flex flex-col justify-center items-center border border-red-600 min-h-screen bg-[url('/noise.svg')] bg-cover bg-center">
       <Card className="max-w-sm w-full rounded-2xl mt-12 h-full ">
-        <CardHeader title="Discuta o seu projeto conosco" />
+        <CardHeader
+          title="Discuta o seu projeto conosco"
+          subheader="Nos conte um pouco sobre sua aplicação web!"
+        />
         <CardContent className="py-0 px-4">
           <BuildMyProjectForm />
         </CardContent>
--- app/dashboard/project-form.tsx
+++ app/dashboard/project-form.tsx
@@ -1,9 +1,19 @@
 "use client";
-import { Button, MenuItem, TextField } from "@mui/material";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material";
 import Form from "next/form";
 import SubmitAction from "./submitAction";
 import { useActionState } from "react";
-import { backEndTechnologies, frontEndTechnologies } from "@/techs";
+import {
+  backEndTechnologies,
+  frontEndTechnologies,
+  databaseList,
+} from "@/techs";
 
 interface Techs {
   id: number;
@@ -13,50 +23,65 @@ interface Techs {
 export default function BuildMyProjectForm() {
   const frontEnd: Techs[] = frontEndTechnologies;
   const [state, formAction, isPending] = useActionState(SubmitAction, null);
+  if (state?.success === false) {
+    console.log();
+  }
   return (
     <Form
       action={formAction}
-      className="flex flex-col border border-red-800 justify-center items-center gap-2"
+      className="flex flex-col justify-center items-center gap-6"
     >
-      <div className="w-full grid grid-cols-2 gap-2 auto-cols-fr">
-        <div className="w-full">
-          <TextField
-            select
-            name="frontend"
-            label="Tecnologias Front-End"
-            defaultValue="React"
-            variant="filled"
-            className="w-full"
-          >
-            {frontEnd.map((front) => (
-              <MenuItem key={front.id} value={front.tech}>
-                {front.tech}
-              </MenuItem>
-            ))}
-          </TextField>
-        </div>
-        <div>
-          <div className="w-full">
-            <TextField
-              select
-              name="frontend"
-              label="Tecnologias Front-End"
-              defaultValue="React"
-              variant="filled"
-              className="w-full"
-            >
-              {backEndTechnologies.map((back) => (
-                <MenuItem key={back.id} value={back.tech}>
-                  {back.tech}
-                </MenuItem>
-              ))}
-            </TextField>
-          </div>
-        </div>
-      </div>
-
-      <Button className=" bg-red-400" disabled={isPending} type="submit">
-        Submit
+      <FormControl fullWidth>
+        <InputLabel id="backend-label">Tecnologias Front-End</InputLabel>
+        <Select
+          variant="outlined"
+          multiple={true}
+          name="frontend"
+          label="Tecnologias Front-End"
+          defaultValue={[]}
+          className="w-full"
+        >
+          {frontEnd.map((front) => (
+            <MenuItem key={front.id} value={front.tech}>
+              {front.tech}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <FormControl fullWidth>
+        <InputLabel id="backend-label">Tecnologias Back-End</InputLabel>
+        <Select
+          multiple={true}
+          name="backend"
+          label="Tecnologias Back-End"
+          defaultValue={[]}
+          className="w-full"
+        >
+          {backEndTechnologies.map((back) => (
+            <MenuItem key={back.id} value={back.tech}>
+              {back.tech}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <FormControl fullWidth>
+        <InputLabel id="databse-label">Banco de dados</InputLabel>
+        <Select
+          multiple={true}
+          name="database"
+          label="banco de dados"
+          defaultValue={[]}
+          className="w-full"
+        >
+          {databaseList.map((option) => (
+            <MenuItem key={option.id} value={option.tech}>
+              {option.tech}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <Button className="bg-red-400" disabled={isPending} type="submit">
+        Enviar Ticket
       </Button>
     </Form>
   );
