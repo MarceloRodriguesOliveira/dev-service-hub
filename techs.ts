0dab98ecbf9bc7969a1d183e94a0331ac9145616
marcelo--rodrigues2@hotmail.com <marcelo--rodrigues2@hotmail.com>
2025-03-14 11:00:56 -0300

    chore: updated available tech list

--- techs.ts
+++ techs.ts
@@ -1,56 +1,56 @@
-export const frontEndTechnologies = [
-  {
-    id: 1,
-    tech: "React",
-  },
-  {
-    id: 2,
-    tech: "Next",
-  },
-  {
-    id: 3,
-    tech: "Vue",
-  },
-  {
-    id: 4,
-    tech: "Angular",
-  },
+export const frontendTechnologies = [
+  { id: 1, name: "React" },
+  { id: 2, name: "Vue.js" },
+  { id: 3, name: "Angular" },
+  { id: 4, name: "Svelte" },
+  { id: 5, name: "Next.js" },
+  { id: 6, name: "Nuxt.js" },
+  { id: 7, name: "Solid.js" },
+  { id: 8, name: "Qwik" },
+  { id: 9, name: "Alpine.js" },
+  { id: 10, name: "Preact" },
 ];
 
-export const backEndTechnologies = [
-  {
-    id: 1,
-    tech: "Node",
-  },
-  {
-    id: 2,
-    tech: "Java Spring",
-  },
-  {
-    id: 3,
-    tech: "NestJs",
-  },
-  {
-    id: 4,
-    tech: "Laravel",
-  },
+export const backendTechnologies = [
+  { id: 1, name: "Node.js" },
+  { id: 2, name: "Spring Boot" },
+  { id: 3, name: "Django" },
+  { id: 4, name: "Flask" },
+  { id: 5, name: "Express.js" },
+  { id: 6, name: "NestJS" },
+  { id: 7, name: "Ruby on Rails" },
+  { id: 8, name: "ASP.NET Core" },
+  { id: 9, name: "FastAPI" },
+  { id: 10, name: "Fiber" },
 ];
 
 export const databaseList = [
-  {
-    id: 1,
-    tech: "MySQL",
-  },
-  {
-    id: 2,
-    tech: "PostgreSQL",
-  },
-  {
-    id: 3,
-    tech: "MongoDB",
-  },
-  {
-    id: 4,
-    tech: "SQLite",
-  },
+  { id: 1, name: "PostgreSQL" },
+  { id: 2, name: "MySQL" },
+  { id: 3, name: "MongoDB" },
+  { id: 4, name: "SQLite" },
+  { id: 5, name: "Redis" },
+  { id: 6, name: "Firebase Firestore" },
+  { id: 7, name: "DynamoDB" },
+  { id: 8, name: "Cassandra" },
+  { id: 9, name: "MariaDB" },
+  { id: 10, name: "CockroachDB" },
+];
+
+export const microservices = [
+  { id: 1, name: "Auth0" },
+  { id: 2, name: "Firebase Authentication" },
+  { id: 3, name: "Stripe" },
+  { id: 4, name: "PayPal" },
+  { id: 5, name: "Twilio" },
+  { id: 6, name: "SendGrid" },
+  { id: 7, name: "AWS Lambda" },
+  { id: 8, name: "Google Cloud Functions" },
+  { id: 9, name: "Datadog" },
+  { id: 10, name: "New Relic" },
+  { id: 11, name: "MongoDB Atlas" },
+  { id: 12, name: "Redis Cloud" },
+  { id: 13, name: "Postmark" },
+  { id: 14, name: "Okta" },
+  { id: 15, name: "Azure Functions" },
 ];
