071d03fbc05751d3992acaf7b09d83a76fe3ec40
marcelo--rodrigues2@hotmail.com <marcelo--rodrigues2@hotmail.com>
2025-03-18 11:37:33 -0300

    feat: submit server action now returns messages based on request status

--- app/dashboard/submitAction.ts
+++ app/dashboard/submitAction.ts
@@ -1,26 +1,50 @@
 "use server";
 
+interface FormDataObject {
+  frontEnd: string[];
+  backEnd: string[];
+  database: string[];
+  microservices?: string[] | false;
+}
+
 export default async function SubmitAction(
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   _prevState: any,
   formData: FormData
 ) {
-  const data = {
-    frontEnd: formData.getAll("frontend") as string[],
-    backEnd: formData.getAll("backend") as string[],
-    database: formData.getAll("database") as string[],
-  };
+  try {
+    const microservices = formData.getAll("microservices") as string[];
+    const data: FormDataObject = {
+      frontEnd: formData.getAll("frontend") as string[],
+      backEnd: formData.getAll("backend") as string[],
+      database: formData.getAll("database") as string[],
+      ...(microservices.length > 0
+        ? { microservices: formData.getAll("microservices") as string[] }
+        : { microservices: false }),
+    };
 
-  console.log("=== Server Action Submit Ticket");
-  console.log(data);
+    console.log("=== Server Action Submit Ticket ===");
+    console.log(data);
 
-  if (
-    data.frontEnd.length === 0 ||
-    data.backEnd.length === 0 ||
-    data.database.length === 0
-  ) {
+    if (
+      data.frontEnd.length === 0 ||
+      data.frontEnd.every((v) => v.trim() === "") ||
+      data.backEnd.length === 0 ||
+      data.backEnd.every((v) => v.trim() === "") ||
+      data.database.length === 0 ||
+      data.database.every((v) => v.trim() === "")
+    ) {
+      return { message: "Preencha todo os campos", success: false };
+    }
+
     return {
-      message: "Preencha todos os campos",
+      message: "Dados Enviados com Sucesso",
+      success: true,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      message: "Ocorreu um erro inesperado. Tente novamente.",
       success: false,
     };
   }
