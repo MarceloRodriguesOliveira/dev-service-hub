0d7254e3333afc84af4c7839311ca33c8e46b4f2
marcelo--rodrigues2@hotmail.com <marcelo--rodrigues2@hotmail.com>
2025-04-02 16:50:29 -0300

    chore: removed unused checkbox input and minor fixes

--- app/dashboard/project-form.tsx
+++ app/dashboard/project-form.tsx
@@ -45,105 +45,95 @@ export default function BuildMyProjectForm() {
   }, [state]);
 
   return (
-    <Form
-      action={formAction}
-      className="flex flex-col justify-center items-center gap-6"
-    >
-      <FormControl fullWidth>
-        <InputLabel id="backend-label">Tecnologias Front-End</InputLabel>
-        <Select
-          variant="outlined"
-          multiple={true}
-          name="frontend"
-          label="Tecnologias Front-End"
-          defaultValue={[]}
-          className="w-full"
-        >
-          {frontEnd.map((front) => (
-            <MenuItem key={front.id} value={front.name}>
-              {front.name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <FormControl fullWidth>
-        <InputLabel id="backend-label">Tecnologias Back-End</InputLabel>
-        <Select
-          multiple={true}
-          name="backend"
-          label="Tecnologias Back-End"
-          defaultValue={[]}
-          className="w-full"
-        >
-          {backendTechnologies.map((back) => (
-            <MenuItem key={back.id} value={back.name}>
-              {back.name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <FormControl fullWidth>
-        <InputLabel id="databse-label">Banco de dados</InputLabel>
-        <Select
-          multiple={true}
-          name="database"
-          label="banco de dados"
-          defaultValue={[]}
-          className="w-full"
-        >
-          {databaseList.map((option) => (
-            <MenuItem key={option.id} value={option.name}>
-              {option.name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      {/* <FormControl fullWidth>
-        <FormLabel>Microserviços</FormLabel>
-        <RadioGroup
-          row
-          aria-labelledby="row-radio-buttons-group-label"
-          name="row-radio-buttons-group"
-          value={isOpen ? "yes" : "no"}
-          onChange={handleRadioChange}
-        >
-          <FormControlLabel value="yes" control={<Radio />} label="Yes" />
-          <FormControlLabel value="no" control={<Radio />} label="No" />
-        </RadioGroup>
-      </FormControl> */}
-      <div className="w-full ">
-        <FormControlLabel
-          className=" p-0 m-0"
-          control={
-            <Checkbox checked={isOpen} onChange={handleCheckboxChange} />
-          }
-          label="Suporte a Microserviços"
-          labelPlacement="start"
-        />
-      </div>
+    <>
+      <Form
+        action={formAction}
+        className="flex flex-col justify-center items-center gap-6"
+      >
+        <FormControl fullWidth>
+          <InputLabel id="backend-label">Tecnologias Front-End</InputLabel>
+          <Select
+            variant="outlined"
+            multiple={true}
+            name="frontend"
+            label="Tecnologias Front-End"
+            defaultValue={[]}
+            className="w-full"
+          >
+            {frontEnd.map((front) => (
+              <MenuItem key={front.id} value={front.name}>
+                {front.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel id="backend-label">Tecnologias Back-End</InputLabel>
+          <Select
+            multiple={true}
+            name="backend"
+            label="Tecnologias Back-End"
+            defaultValue={[]}
+            className="w-full"
+          >
+            {backendTechnologies.map((back) => (
+              <MenuItem key={back.id} value={back.name}>
+                {back.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel id="databse-label">Banco de dados</InputLabel>
+          <Select
+            multiple={true}
+            name="database"
+            label="banco de dados"
+            defaultValue={[]}
+            className="w-full"
+          >
+            {databaseList.map((option) => (
+              <MenuItem key={option.id} value={option.name}>
+                {option.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
 
-      <FormControl fullWidth>
-        <InputLabel id="databse-label">Microserviços</InputLabel>
-        <Select
-          multiple={true}
-          disabled={!isOpen}
-          name="microservices"
-          label="microservicos"
-          defaultValue={[]}
-          className="w-full"
-        >
-          {microservices.map((option) => (
-            <MenuItem key={option.id} value={option.name}>
-              {option.name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+        <FormControl fullWidth>
+          <InputLabel id="databse-label">Microserviços</InputLabel>
+          <Select
+            multiple={true}
+            //disabled={!isOpen}
+            name="microservices"
+            label="microservicos"
+            defaultValue={[]}
+            className="w-full"
+          >
+            {microservices.map((option) => (
+              <MenuItem key={option.id} value={option.name}>
+                {option.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
 
-      <Button className="bg-red-400" disabled={isPending} type="submit">
-        Enviar Ticket
-      </Button>
+        <Button
+          sx={{
+            backgroundColor: "#191a23",
+            color: "white",
+            "&:hover": {
+              backgroundColor: "#22232d",
+            },
+          }}
+          className="w-full  text-white text-center text-base transition ease-in"
+          disabled={isPending}
+          type="submit"
+        >
+          Enviar Ticket
+        </Button>
+      </Form>
       <ToastContainer />
-    </Form>
+    </>
   );
 }
